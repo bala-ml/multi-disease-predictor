78383ddd08da8549f1fe5dd205a48216fc4cf4c3
bala-ml <balaji.analytics2k@gmail.com>
2026-03-17 14:21:37 +0530

    added frontend streamlit

---
+++ src/frontend/app.py
@@ -0,0 +1,35 @@
+import sys
+from pathlib import Path
+
+PROJECT_ROOT = Path(__file__).resolve().parents[3]
+sys.path.append(PROJECT_ROOT)
+
+import streamlit as st
+
+st.set_page_config(
+    page_title="Multi-Disease Predictor",
+    page_icon="🩺",
+    layout="centered"
+)
+
+st.markdown("""
+<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+
+<style>
+* {
+    font-family: 'Poppins', sans-serif !important;
+}
+</style>
+""", unsafe_allow_html=True)
+
+st.title("🧠Multi-Disease Predictor")
+
+st.write(
+    """
+Use the left sidebar to navigate:
+- 🩺 Diabetes Risk Predictor
+- ❤️ Cardio Risk Predictor
+"""
+)
+
+st.info("Make sure the FastAPI backend is running before using predictions.")
